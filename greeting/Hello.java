e70fcd309a977fc45bb6bf0ccceb857214083c27
Chris Campell <campellcl@appstate.edu>
2017-02-02 19:54:37 -0500

    Implemented command line argument enhancement.

--- greeting/Hello.java
+++ greeting/Hello.java
@@ -3,7 +3,13 @@ import java.io.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String username = "";
+        if (args.length != 0) {
+            username = args[0]; 
+            System.out.printf("Hello, %s!\n", username);
+        } else {
+            System.out.println("Hello, Chris!");
+        }
     }
 }
 
